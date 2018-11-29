71da0fd127a2194418d852186d0e435d4a7888b4
Adam <graha.asa@gmail.com>
2018-11-29 19:12:40 +0700

    fix heroku

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
-app.listen(port,'0.0.0.0', () => {
+app.listen(port, () => {
   console.log('Listening on port', port);
 });
 
