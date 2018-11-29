a161752b501300a63e70be013bca255694f28345
Adam <graha.asa@gmail.com>
2018-11-29 19:14:09 +0700

    fix port heroku

--- index.js
+++ index.js
@@ -38,8 +38,7 @@ function errorHandler(err, req, res, next) {
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Listening on port', port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
